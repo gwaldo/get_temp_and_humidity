338caa5a9c9ab25e0f160c0bf557de8e1acb1c96
Waldo G <gwaldo@gmail.com>
2021-10-15 13:12:50 -0400

    fixing the agent to actually grab metrics at the appropriate time, and adding fahrenheit

--- get_temp_and_humidity.py
+++ get_temp_and_humidity.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-# Note: Ensure that the user running this script is a member of the `gpio` group.
+# Note: Ensure that the user running this script is a member of the `gpio` or `dialout` group.
 # sudo usermod -a -G gpio dd-agent
 # Note: Install the Adafruit_DHT module to the DD embedded python
 # sudo -Hu dd-agent /opt/datadog-agent/embedded/bin/pip3 install Adafruit_DHT
@@ -8,21 +8,36 @@
 from datadog_checks.base import AgentCheck
 import Adafruit_DHT
 
+__version__ = "1.1.0"
+
 DHT_SENSOR = Adafruit_DHT.DHT22
 OUTSIDE_DHT_PIN = 4  # GPIO4 is pin 7
 INSIDE_DHT_PIN = 24  # GPIO22 is pin 15
 
-inside_humidity, inside_temperature = Adafruit_DHT.read_retry(
-    DHT_SENSOR, INSIDE_DHT_PIN, retries=3, delay_seconds=2
-)
-outside_humidity, outside_temperature = Adafruit_DHT.read_retry(
-    DHT_SENSOR, OUTSIDE_DHT_PIN, retries=3, delay_seconds=2
-)
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
 
 
 class TempHumidityCheck(AgentCheck):
     def check(self, instance):
-        self.gauge("home.inside.temp", inside_temperature)
+        inside_humidity, inside_temperature_c = Adafruit_DHT.read_retry(
+            DHT_SENSOR, INSIDE_DHT_PIN, retries=3, delay_seconds=2
+        )
+        outside_humidity, outside_temperature_c = Adafruit_DHT.read_retry(
+            DHT_SENSOR, OUTSIDE_DHT_PIN, retries=3, delay_seconds=2
+        )
+
+        # if inside_humidity is not None:
+        # do inside_humidity things
+        # repeat...
+
+        inside_temperature_f = celsius_to_fahrenheit(inside_temperature_c)
+        outside_temperature_f = celsius_to_fahrenheit(outside_temperature_c)
+
+        self.gauge("home.inside.temp.c", inside_temperature_c)
+        self.gauge("home.inside.temp.f", inside_temperature_f)
         self.gauge("home.inside.humidity", inside_humidity)
-        self.gauge("home.outside.temp", outside_temperature)
+        self.gauge("home.outside.temp.c", outside_temperature_c)
+        self.gauge("home.outside.temp.f", outside_temperature_f)
         self.gauge("home.outside.humidity", outside_humidity)
